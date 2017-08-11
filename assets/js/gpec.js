852cc3867ffbaa7db31cd2b01324b45cf3e841f6
Gabriel Domanowski <g.domanowski@agridata.eu>
2017-08-11 12:21:44 +0200

    Add script

---
+++ assets/js/gpec.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+    var city = {
+        init : function() {
+            var active_city=$("#city").val();
+            var _array=[];
+            $(`#street_list option`).each(function(indx, elem){
+                $(elem).hide();
+            });
+            $(`#street_list option[data-city=${active_city}]`).each(function(indx, elem){
+                $(elem).show();
+            });
+        }
+    }
+
+    city.init();
+    $("city").on("change",function(){city.init()});
+})
