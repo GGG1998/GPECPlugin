961e157bee79e01a496403430bbabcd914d4d262
Gabriel Domanowski <g.domanowski@agridata.eu>
2017-08-11 12:20:21 +0200

    Add controller

--- gpec-calc.php
+++ gpec-calc.php
@@ -15,23 +15,30 @@ if ( !class_exists( 'WP_gpec' ) ) {
 
     class WP_gpec {
         private $gpec_model;
+        private $gpec_controller;
 
         public function __construct() {
             include_once(dirname(__DIR__).'/gpec-calc/controller/gpec_controller.php');
             include_once(dirname(__DIR__).'/gpec-calc/model/gpec_model.php');
             
+            $this->gpec_model=new gpec_model();
+            $this->gpec_controller=new gpec_controller($this->gpec_model);
+
             $this->_hooks_load();
         }
 
         public function gpec_calc_views() {
-            ob_start();
-            require_once(dirname(__DIR__).'/gpec-calc/templates/form.html');
-            $view=ob_get_clean();
-            
-            return $view;
+            $cities=$this->gpec_model->getCities();
+            $streets=$this->gpec_model->getStreets();
+
+            require_once(dirname(__DIR__).'/gpec-calc/templates/form.php');
         }
 
         private function _hooks_load() {
+            $this->gpec_controller->init();
+
+            wp_enqueue_script( 'custom-script', get_option('siteurl').'/wp-content/plugins/gpec-calc/assets/js/gpec.js', array( 'jquery' ) );
+
             add_shortcode('gpec_calc',array($this,'gpec_calc_views'));
         }
     }
