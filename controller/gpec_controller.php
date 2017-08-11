15713cb5f7517efe5c1fd29e7ea4833df1f7e4de
Gabriel Domanowski <g.domanowski@agridata.eu>
2017-08-11 12:21:13 +0200

    Add some business logic

--- controller/gpec_controller.php
+++ controller/gpec_controller.php
@@ -1,7 +1,26 @@
 <?php
 if ( !class_exists( 'gpec_controller' ) ) {
     class gpec_controller {
-        
+        private $gpec_model;
+
+        public function __construct($_model) {
+            $this->gpec_model=$model;
+        }
+        public function init() {
+            add_action( 'rest_api_init', function () {
+                register_rest_route( 'gpec/v1', '/street/(?P<city_name>\w+)/', array(
+                    'methods' => 'GET',
+                    'callback' => array($this,'getStreets'),
+                ) );
+            } );
+        }
+        public function getStreets($data) {
+            $streets=$this->gpec_model->getStreetsByCity($data['city_name']);
+            $arr=array();
+            foreach($streets as $street)
+                array_push($arr, $street->{'street'});
+            return json_encode($arr);
+        }
     }
 }
 ?>
--- model/gpec_model.php
+++ model/gpec_model.php
@@ -1,7 +1,26 @@
 <?php
 if ( !class_exists( 'gpec_model' ) ) {
     class gpec_model {
-        
+        private $db;
+        public function __construct() {
+            global $wpdb;
+            $this->db=$wpdb;
+        }
+
+        public function getCities() {
+            $query="SELECT DISTINCT city FROM gpec_client";
+            return $this->db->get_results($query,OBJECT);
+        }
+
+        public function getStreets() {
+            $query='SELECT DISTINCT street,city FROM gpec_client';
+            return $this->db->get_results($query, OBJECT);
+        }
+
+        public function getStreetsByCity($city_name) {
+            $query='SELECT DISTINCT street FROM gpec_client WHERE city="'.$city_name.'"';
+            return $this->db->get_results($query, OBJECT);
+        }
     }
 }
 ?>
