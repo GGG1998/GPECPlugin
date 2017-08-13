38637e04f9e900fd65375ad55607c43091b39753
GGG1998 <gabrys19@onet.eu>
2017-08-13 22:56:22 +0200

    Fix some bugs from controller; Save clients db

--- controller/gpec_import_controller.php
+++ controller/gpec_import_controller.php
@@ -16,7 +16,7 @@ if(!class_exists('gpec_import_controller')) {
         public function add_import_page() {
             add_options_page(
                 'Ustawienia GPEC',
-                'Import bazy danych',
+                'Import GPEC',
                 'manage_options',
                 'import-db-gpec',
                 array($this, 'create_import_page')
@@ -37,7 +37,7 @@ if(!class_exists('gpec_import_controller')) {
             $this->_validateField($file,"list_rules","listy reguł spinających");
             $this->_validateField($file,"list_costs","listy ceny taryf");
             if(count($this->error)==0) {
-
+                $this->importClient($file['list_clients']);
             }else {
                 $error=$this->error;
                 require_once(dirname(__DIR__).'/templates/error.php');
@@ -46,22 +46,59 @@ if(!class_exists('gpec_import_controller')) {
 
         }
 
+        public function importClient($file) {
+            if(empty($file['name'])) return;
+            $required_field=array('city','street','number_flat','number_home','group_client','company');
+
+            $clients=fopen($file['tmp_name'],"r") or die("Nie mogę otworzyć pliku");
+            while (($data = fgetcsv($clients, 0, ";")) !== FALSE) {
+                $num = count($data);
+                $row++;
+                if($row==1) {
+                    if($this->_checkRequairment($required_field,$data,$num)==false)
+                        break;
+                }else {
+                    $client_model=$this->manager->client;
+                    $client_model->setCity($data[0]);
+                    $client_model->setStreet($data[1]);
+                    $client_model->setNumberFlat($data[2]);
+                    $client_model->setNumberHome($data[3]);
+                    $client_model->setGroupClient($data[4]);
+                    $client_model->setCompany($data[5]);
+                    $client_model->save();
+                    echo '<p style="color:green;">Dodałem bazę klientów do bazy</p>';
+                }
+            }
+            fclose($clients);
+        }
+
         private function _check_format($format) {
-            return $format=="csv" ? true : false;
+            $format_list = array('application/vnd.ms-excel','text/plain','text/csv','text/tsv');
+            return in_array($format,$format_list) ? true : false;
         }
 
         private function _push_error($err) { array_push($this->error,$err); }
+        
         private function _clear_error() { $this->error=array(); }
 
         private function _validateField($file,$key,$name) {
             if(isset($file[$key])) {
-                if(empty($file[$key]['name']))
-                    $this->_push_error("Nie wybrałeś pliku dla ".$name);
-                if($this->_check_format($file[$key]['type'])==false)
-                    $this->_push_error("Nieprawidłowy format pliku");
+                if(!empty($file[$key]['name'])) {
+                    if($this->_check_format($file[$key]['type'])==false)
+                        $this->_push_error("Nieprawidłowy format pliku");
+                }
             }else
                 $this->_push_error("Nie wybrałeś pliku dla ".$name);
         }
+
+        private function _checkRequairment($req, $data, $nr) {
+            for($i=0; $i<$nr; $i++) {
+                if(!empty($data[$i]))
+                    if(!in_array($data[$i],$req)) return false; 
+            }
+            return true;
+        }
+
     }
 }
 ?>
